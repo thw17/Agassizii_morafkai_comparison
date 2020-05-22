4ae62dedf64e7cf6feb0785c9dff279c38d1c1f6
Tim Webster <twebster17@gmail.com>
2020-05-22 01:30:58 -0600

    update samples

---
+++ scripts/symlinks.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/Gber59507_R1.fastq.gz Gber59507_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/Gber59507_R2.fastq.gz Gber59507_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM005f_R1.fastq.gz HSEM005f_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM005f_R2.fastq.gz HSEM005f_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM008f_R1.fastq.gz HSEM008f_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM008f_R2.fastq.gz HSEM008f_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM012f_R1.fastq.gz HSEM012f_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM012f_R2.fastq.gz HSEM012f_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM014e_R1.fastq.gz HSEM014e_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM014e_R2.fastq.gz HSEM014e_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM016f_R1.fastq.gz HSEM016f_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM016f_R2.fastq.gz HSEM016f_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM017f_R1.fastq.gz HSEM017f_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM017f_R2.fastq.gz HSEM017f_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM018f_R1.fastq.gz HSEM018f_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM018f_R2.fastq.gz HSEM018f_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM019f_R1.fastq.gz HSEM019f_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM019f_R2.fastq.gz HSEM019f_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM021C_R1.fastq.gz HSEM021C_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM021C_R2.fastq.gz HSEM021C_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM022F_R1.fastq.gz HSEM022F_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/HSEM022F_R2.fastq.gz HSEM022F_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM10-002_S1_L007_R1_001.fastq.gz PM10_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM10-002_S1_L007_R2_001.fastq.gz PM10_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM108-004_S2_L007_R1_001.fastq.gz PM108_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM108-004_S2_L007_R2_001.fastq.gz PM108_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM112-005_S3_L007_R1_001.fastq.gz PM112_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM112-005_S3_L007_R2_001.fastq.gz PM112_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM4-006_S4_L007_R1_001.fastq.gz PM4_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM4-006_S4_L007_R2_001.fastq.gz PM4_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM5-012_S5_L007_R1_001.fastq.gz PM5_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/PM5-012_S5_L007_R2_001.fastq.gz PM5_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB16-002_S6_L008_R1_001.fastq.gz WSB16_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB16-002_S6_L008_R2_001.fastq.gz WSB16_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB161-004_S7_L008_R1_001.fastq.gz WSB161_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB161-004_S7_L008_R2_001.fastq.gz WSB161_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB50-005_S8_L008_R1_001.fastq.gz WSB50_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB50-005_S8_L008_R2_001.fastq.gz WSB50_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB57-006_S9_L008_R1_001.fastq.gz WSB57_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB57-006_S9_L008_R2_001.fastq.gz WSB57_R2.fastq.gz
+
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB77-012_S10_L008_R1_001.fastq.gz WSB77_R1.fastq.gz
+ln -s /uufs/chpc.utah.edu/common/home/webster-group1/Gopherus/WSB77-012_S10_L008_R2_001.fastq.gz WSB77_R2.fastq.gz
